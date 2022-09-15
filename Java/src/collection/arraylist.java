b257788dd57f95ec26857b0646e210be34512d9f
saraf <saraf@LAPTOP-528K5AC6>
2022-09-15 22:39:20 +0530

    Please review

---
+++ Java/src/collection/arraylist.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class arraylist {
+public static void main(String[] args) {
+	
+	ArrayList al	= new ArrayList();
+	
+	al.add("Aniket");
+	al.add(10);
+	System.out.println(al);
+}
+}
+	
